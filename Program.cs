8c5266f871425774e16ddecbccf5d4907c13f68b
randywhitaker <20479914+randywhitaker@users.noreply.github.com>
2020-01-09 21:24:27 -0600

    Add Console applications

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace watchmen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread thr = new Thread(job1);
+            thr.Start();
+
+            Console.WriteLine("Main job completed!");
+            Console.ReadLine();
+        }
+        static void job1()
+        {
+            int counter = 0;
+            do
+            {
+                counter++;
+                Console.WriteLine("My Job is running");
+            } while(counter < 5);
+        }
+    }
+}
